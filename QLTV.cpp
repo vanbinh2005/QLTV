f8e70092f54f78e06328adc1fc4ded3ab8dfb480
Thethien2k5 <thienobita0203@gmail.com>
2023-12-25 21:15:47 +0700

    Add QLTV.cpp file

---
+++ QLTV.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>   //khả năng đọc chuỗi tốt hơn
+#include <cmath>    //Thư viện toán
+#include <stdlib.h> //quản lý bộ nhớ
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
